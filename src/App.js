ae043a33d5e527d96729feb886bbb81ff9583759
jeyansaran620 <jeyansaran620@gmail.com>
2020-09-02 19:32:35 +0530

    Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
+
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  render()
+  {
   return (
     <div className="App">
      <Navbar dark color="primary">
@@ -11,9 +21,10 @@ function App() {
          <NavbarBrand href="/">Jeyan Saran</NavbarBrand>
        </div>
      </Navbar>
-     <Menu />
+     <Menu menu={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
